0dac1a7597ba08ec66f502437a0facf491f7a26b
Isaac Luy <isaacluy@github.com>
2020-12-10 16:52:39 -0500

    Added <LanguageSelector/> to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import LanguageSelector from './components/language-selector';
 import Row from 'react-bootstrap/Row';
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     <Container fluid>
       <Row className="justify-content-md-center">
         <h1 className="text-center">Welcome to the Mask Trainer</h1>
+      </Row>
+      <Row className="justify-content-md-center">
+        <LanguageSelector />
       </Row>
     </Container>
   );
