449225673c35dfccf98a6b3235da4180144f2f2f
Isaac Luy <isaacluy@github.com>
2020-12-11 12:13:47 -0500

    Added static data file for the App

---
+++ src/reducers/data.js
@@ -0,0 +1,20 @@
+export const languageListReducer = () => {
+    return [
+        {
+            code: 'en',
+            text: 'English'
+        },
+        {
+            code: 'es',
+            text: 'Español'
+        },
+        {
+            code: 'pt',
+            text: 'Português'
+        },
+        {
+            code: 'fr',
+            text: 'Français'
+        }
+    ];
+};
