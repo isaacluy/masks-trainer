f63bff4fdcbc4189b059fe8328bfd9cbfc1c301c
Isaac Luy <isaacluy@github.com>
2020-12-11 17:40:29 -0500

    Updated <App>. Made the app get the selected language from the URL and call the selectLanguage reducer to update the state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import LanguageSelector from './components/LanguageSelector';
 import Row from 'react-bootstrap/Row';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <Container fluid>
-      <Row className="justify-content-md-center">
-        <h1 className="text-center">Welcome to the Mask Trainer</h1>
-      </Row>
-      <Row className="justify-content-md-center">
-        <LanguageSelector />
-      </Row>
-    </Container>
-  );
+import LanguageSelector from './components/LanguageSelector';
+import { selectLanguage } from './actions';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    if(window.location.pathname && window.location.pathname !== '/') {
+      props.selectLanguage(window.location.pathname);
+    }
+  }
+
+  render = () => {
+    return (
+      <Container fluid>
+        <Row className="justify-content-md-center">
+          <h1 className="text-center">Welcome to the Mask Trainer</h1>
+        </Row>
+        <Row className="justify-content-md-center">
+          <LanguageSelector />
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return { 
+        languages: state.languages,
+        selectedLanguage: state.selectedLanguage
+    }
+}
+
+export default connect(
+  mapStateToProps,
+  { selectLanguage }
+  )(App);
