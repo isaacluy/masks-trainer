bfb219adb5d6daa6a109b354523e42598271a2c1
Isaac Luy <isaacluy@github.com>
2020-12-11 13:16:47 -0500

    Updated <LanguageSelector>. When user selects a language it changes the URL without doing a full page reload

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -4,14 +4,23 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import { connect } from 'react-redux';
 
 class LanguageSelector extends React.Component {
+    onClick(event) {
+        event.preventDefault();
+
+        const href = event ? event.target.attributes.href.value : '/';
+        
+        window.history.pushState({}, '', href);
+    }
+
     renderDropdownItems() {
         return this.props.languages.map((language, index) => {
             return (
-            <Dropdown.Item href={`/${language.code}`} key={index}>
+            <Dropdown.Item onClick={this.onClick} href={`/${language.code}`} key={index}>
                 {language.text}
             </Dropdown.Item>)
         });
     }
+
     render() {
         return (
             <DropdownButton id="language-selection-button" title="Select a Language">
