a6168d713e63ebd7c35ff143e19061168ccc172d
Isaac Luy <isaacluy@github.com>
2020-12-11 12:17:50 -0500

    Updated the <LanguageSelector> to use Redux state to populate the list of languages available

--- src/components/language-selector.js
+++ src/components/language-selector.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import { connect } from 'react-redux';
 
 class LanguageSelector extends React.Component {
+    renderDropdownItems() {
+        return this.props.languages.map((language, index) => {
+            return (
+            <Dropdown.Item href={`/${language.code}`} key={index}>
+                {language.text}
+            </Dropdown.Item>)
+        });
+    }
     render() {
         return (
             <DropdownButton id="language-selection-button" title="Select a Language">
-                <Dropdown.Item href="/en">English</Dropdown.Item>
-                <Dropdown.Item href="/es">Español</Dropdown.Item>
-                <Dropdown.Item href="/pt">Portugues</Dropdown.Item>
-                <Dropdown.Item href="/fr">Français</Dropdown.Item>
+                {this.renderDropdownItems()}
             </DropdownButton>
         );
     }
 }
 
-export default LanguageSelector;
+const mapStateToProps = state => {
+    return { languages: state.languages}
+}
+
+export default connect(mapStateToProps)(LanguageSelector);
