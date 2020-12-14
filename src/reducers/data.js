9ee3ba5d42037ac03aec3aec29955cc515848ec8
Isaac Luy <isaacluy@github.com>
2020-12-14 16:44:22 -0500

    Updated the data reducer to include the experience and innocence texts

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -3,22 +3,30 @@ export const languageListReducer = () => {
         {
             href: '/en',
             text: 'English',
-            welcomeMessage: 'Welcome to the Masks Trainer'
+            welcomeMessage: 'Welcome to the Masks Trainer',
+            experience: 'Experience',
+            innocence: 'Innocence'
         },
         {
             href: '/es',
             text: 'Español',
-            welcomeMessage: 'Bienvenido al Entrenador de Máscaras'
+            welcomeMessage: 'Bienvenido al Entrenador de Máscaras',
+            experience: 'Experiencia',
+            innocence: 'Inocencia'
         },
         {
             href: '/pt',
             text: 'Português',
-            welcomeMessage: 'Bem-vindo ao Treinador de Máscaras'
+            welcomeMessage: 'Bem-vindo ao Treinador de Máscaras',
+            experience: 'Experiência',
+            innocence: 'Inocência'
         },
         {
             href: '/fr',
             text: 'Français',
-            welcomeMessage: 'Bienvenue au formateur de masques'
+            welcomeMessage: 'Bienvenue au formateur de masques',
+            experience : 'Expérience',
+            innocence : 'Innocence'
         }
     ];
 };
