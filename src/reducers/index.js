1a639b92534991d5172645c0aae5c1de530a8529
Isaac Luy <isaacluy@github.com>
2020-12-15 17:16:15 -0500

    Added selected masks reducer for add and remove mask (only for redux refresher)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,9 +7,27 @@ const selectedLanguageReducer = (selectedLangHref = '/', action) => {
     }
 
     return selectedLangHref;
-};
+}
+
+const selectedMasksReducer = (masks = [], action) => {
+    if(action && action.type === 'ADD_MASK') {
+        let newMasks = Array.from(masks);
+        newMasks.push(action.payload);
+        return newMasks;
+    } else if (action && action.type === 'REMOVE_MASK') {
+        const index = masks.indexOf(action.payload);
+        if (index > -1) {
+            const newMasks = Array.from(masks);
+            newMasks.splice(index, 1);
+            return newMasks;
+        }
+    }
+
+    return masks;
+}
 
 export default combineReducers({
     languages: languageListReducer,
+    masks: selectedMasksReducer,
     selectedLanguage: selectedLanguageReducer
 });
