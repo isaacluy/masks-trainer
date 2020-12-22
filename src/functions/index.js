feea9606dbab0d959faca854c5750440c356f36b
Isaac Luy <isaacluy@github.com>
2020-12-22 16:37:35 -0500

    ! functions needed for the last commit

--- src/functions/index.js
+++ src/functions/index.js
@@ -33,11 +33,36 @@ export const buildMasksIdsArray = languageObj => {
     return masksIdsArray;
 }
 
-export const getSelectedLanguage = (languages, href) => {
-    return languages.find(language => language.href === href);
+export const getRandomMask = masksNames => {
+    const numberOfMasks = masksNames.length;
+    const randomMaskIndex = randomIntFromInterval(0, numberOfMasks-1);
+
+    return masksNames[randomMaskIndex];
 }
 
 export const getPropertyFromLanguageObject = 
     (currentlySelectedLanguage, property, defaultPropertyValue) => {
         return !currentlySelectedLanguage ? defaultPropertyValue : currentlySelectedLanguage[property];
     }
+
+export const getSelectedLanguage = (languages, href) => {
+    return languages.find(language => language.href === href);
+}
+
+export const maskNameConverter = (maskId, selectedLanguage) => {
+    const number = maskId.slice(0,1);
+    const type = maskId.slice(2);
+    let maskName = '';
+
+    if (selectedLanguage.href === '/en') {
+        maskName = `${number} ${selectedLanguage[type]}`;
+    } else {
+        maskName = `${selectedLanguage[type]} ${number}`;
+    }
+
+    return maskName;
+}
+
+export const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
